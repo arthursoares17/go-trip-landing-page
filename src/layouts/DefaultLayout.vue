721ebd7384dbd33b0ce903f97b956a6ccdd8cec4
Arthur Soares <arthursoares0217@gmail.com>
2025-08-07 01:13:33 -0300

    first commit

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="relative">
+    <main class="size-full">
+      <RouterView/>
+    </main>
+  </div>
+</template>
