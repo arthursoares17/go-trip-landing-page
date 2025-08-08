90483234177d028a9d1960bb02db21fa1bcfa41f
Arthur Soares <arthursoares0217@gmail.com>
2025-08-08 00:49:14 -0300

    feat: add Logo component

---
+++ src/components/globals/Logo.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import { RouterLink } from 'vue-router'
+  import logoImage from '@/assets/images/logo-image.webp'
+</script>
+
+<template>
+  <RouterLink to="/">
+    <div class="flex items-center space-x-2">
+      <img  
+        class="size-full"
+        :src="logoImage" 
+        alt="GoTrip logo"
+      />
+
+      <span class="text-base font-medium">
+        GoTrip
+      </span>
+    </div>
+  </RouterLink>
+</template>
