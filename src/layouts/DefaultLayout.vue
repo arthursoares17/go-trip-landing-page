4808acf4c3bc4cefd5136b88b6076b419cd40db8
Arthur Soares <arthursoares0217@gmail.com>
2025-08-08 17:19:03 -0300

    feat: integrate Header component in DefaultLayout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
   import { RouterView } from 'vue-router'
+  import Header from '@/components/globals/Header.vue'
 </script>
 
 <template>
   <div class="relative">
+    <Header/>
+    
     <main class="size-full">
       <RouterView/>
     </main>
