a877b9c6b7cfc5375ed3899bfa0c89627c0c4b93
Arthur Soares <arthursoares0217@gmail.com>
2025-08-08 20:49:19 -0300

    feat: add Button component

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import { computed } from 'vue'
+
+  interface Props {
+    variant: keyof typeof buttonVariants
+  }
+
+  const props = defineProps<Props>()
+
+  const buttonVariants = {
+    primary: 'bg-primary text-primary-foreground py-3 px-4 rounded-xl hover:bg-primary-hover'
+  } as const
+
+  const buttonClass = computed(() => {
+    return buttonVariants[props.variant]
+  })
+</script>
+
+<template>
+  <button
+    class="cursor-pointer flex justify-center items-center text-sm font-medium transition duration-200"
+    :class="buttonClass"
+  >
+    <slot/>
+  </button>
+</template>
