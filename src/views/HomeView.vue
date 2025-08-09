8f874572dd93f23fecc1f0acb3f819cd44687463
Arthur Soares <arthursoares0217@gmail.com>
2025-08-09 00:40:04 -0300

    feat: integrate DestinationsSection component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
   import HeroSection from '@/components/sections/hero/HeroSection.vue'
+  import DestinationsSection from '@/components/sections/destinations/DestinationsSection.vue'
 </script>
 
 <template>
   <HeroSection/>
+  <DestinationsSection/>
 </template>
